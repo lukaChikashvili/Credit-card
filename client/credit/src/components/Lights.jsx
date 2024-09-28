2c060d559063a4635d1f1760d8cf4a09bee6c4bc
luka chikashvili <lukachikashvili2020@gmail.com>
2024-09-28 19:24:14 +0400

    add credit card textures

--- client/credit/src/components/Lights.jsx
+++ client/credit/src/components/Lights.jsx
@@ -17,9 +17,9 @@ const Lights = () => {
     shadow-camera-left={ - 10 }
 />
 
-<pointLight position={[10, 10, 10]} />
+<pointLight position={[0, 20, 30]} />
 
-<ambientLight intensity={3} />
+
   </>
   )
 }
--- client/credit/src/components/Model.jsx
+++ client/credit/src/components/Model.jsx
@@ -1,14 +1,48 @@
-import { useGLTF } from '@react-three/drei'
-import React from 'react'
+import { useGLTF, useTexture } from '@react-three/drei'
+import React, { useEffect, useRef } from 'react'
+import red from '../assets/red.jpg';
+import { MeshPhysicalMaterial } from 'three';
+
 
 const Model = () => {
    
     const model = useGLTF('./credit_card.glb');
 
+    let card = useRef();
 
+    // credit card texture
+    const texture = useTexture(red);
+     
+    useEffect(() => {
+     
+
+        if(card.current) {
+            card.current.traverse((child) => {
+                if(child.isMesh) {
+                    const newMaterial = new MeshPhysicalMaterial({
+                        map: texture,
+                        ior: 2,
+                        metalness: 0.4,
+                        reflectivity: 0.9,
+                        iridescenceIOR: 0.5,
+                        sheen: 0.8,
+                        clearcoat: 1,
+                        flatShading: true,
+                        
+                       
+                    });
+                    child.material = newMaterial;
+                    child.material.needsUpdate = true;
+                    child.material.metalness = 0.02;
+                    console.log(child.material)
+
+                }
+            })
+        }
+    }, [texture])
   return (
       <>
-        <primitive object={model.scene} scale = {30}/>
+        <primitive object={model.scene} scale = {50} ref = {card} />
       </>
   )
 }
